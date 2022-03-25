7e8a06537b3baba737b2846c6520352b22c67cd2
ScriptErrorVGM <mixajlov.grigory@gmail.com>
2022-03-25 13:02:09 +1000

    Update list_dataframe.py

--- homework2/list_dataframe.py
+++ homework2/list_dataframe.py
@@ -1,6 +1,7 @@
 from asyncio.windows_events import NULL
 import numpy as np 
 import pandas as pd 
+import math
 
 data = [["Вжик", "Zipper the Fly", "fly", "0.7"],
         ["Гайка", "Gadget Hackwrench", "mouse", None],
@@ -11,9 +12,9 @@ data = [["Вжик", "Zipper the Fly", "fly", "0.7"],
 # nuber 1
 
 df = pd.DataFrame(data)
-print(df[1])
-df.columns = ['ru_name','en_name','class','cheer']
-df['cheer'] = df['cheer'].astype(float)
+#print(df[1])
+#df.columns = ['ru_name','en_name','class','cheer']
+df[3] = df[3].astype(float)
 #print(df[1])
 
 assert type(df) == pd.core.frame.DataFrame, "Объект df не является датафреймом"
@@ -30,15 +31,39 @@ assert N == 5, "Неверный ответ"
 # number 3
 
 dff = df
-dff['cheer'] = dff['cheer'].fillna('None')
-cn = len(dff[dff.cheer != 'None'])
+dff[3] = dff[3].fillna('None')
+cn = len(dff[dff[3] != 'None'])
 
 assert cn == 4, "Неверный ответ"
 
 # number 4
 
-g = df['en_name'][2]
+
+g = df[1][2]
 
 assert g == 'Dale', "Неверный ответ"
 
+# number 5
 
+
+df1 = df.iloc[1:4,0:3]
+
+assert df1.shape == (3, 3), "Неверное число строк и столбцов"
+assert df1[1].values[-2] == "Dale", "Выбраны не те строки или столбцы"
+
+# number 6
+
+
+
+df.columns = ['ru_name','en_name','class','cheer']
+
+assert df1.shape == (3,3)
+assert df1[1].values[-2] == 'Dale', 'Выбраны не те строки или столбцы'
+
+# nuber 7
+
+df.insert('logcheer', np.log(df['cheer']))
+
+print(df)
+
+  
